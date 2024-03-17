e1e2070b685d835f2f447bf19518a907fa940a12
Don Dang <doncdang@hotmail.com>
2024-03-16 23:11:09 -0700

    feat: update code to find shortest path using Dijkstra's algorithm

--- main.py
+++ main.py
@@ -24,11 +24,13 @@ import collections  # special data structures
 class Place:
     def __init__(self):
         """
-        create dictionaries to hold graph, cheapest path and cheapest prior stopped locations
+        create dictionary to hold starting location to its neighbors
+        create dictionary to hold cheapest path from starting location to adjacent station
+        create dictionary to hold cheapest previous stopped locations
         """
-        self.adjacency_list = {}  # adjacency list to hold vertices and their neighbors and weights
-        self.cheapest_prices_table = {}  # hold the cheapest prices starting location
-        self.cheapest_previous_stopped_station = {}  # hold the cheapest prior stopped location
+        self.adjacency_list = {}  # adjacency list to hold starting location and their neighbors and travel cost
+        self.cheapest_prices_table = {}  # hold the cheapest prices from starting location to adjacent station
+        self.cheapest_previous_stopped_station = {}  # hold the cheapest previous stopped location
 
     @staticmethod
     def create_graph(graph_config):
@@ -90,6 +92,7 @@ class Place:
         visited = set()  # create empty set to hold visited locations
         q = collections.deque()  # create a double-ended queue
         q.appendleft(start_loc)  # add starting location to queue
+        self.cheapest_prices_table[start_loc] = 0  # set starting location as first key and set travel cost to itself $0
         while q:
             current_loc = q.pop()  # pop queue and make starting location as current location
             self.visited_location(current_loc, visited)  # mark current location as visited
@@ -107,10 +110,6 @@ class Place:
             visited.add(vertex)
             print(f"{vertex}", end=" ")
 
-        # if vertex not in visited or visited[vertex] is False:
-        #     visited[vertex] = True
-        #     print(f"{vertex}", end=" ")
-
     def enqueue_unvisited_locations(self, current_loc, visited, que):
         """
         look for destination that is connected to current location
@@ -123,11 +122,47 @@ class Place:
         """
         if current_loc not in self.adjacency_list:  # if current location is not found in adjacency list, return ""
             return
-        destinations = self.adjacency_list[current_loc]  # get neighbors value list from current location (key)
-        for destination in destinations:  # get destination from destinations value list
-            # print(f"destination: {destination}")
-            if destination not in visited:  # if destination is not marked visited
-                que.appendleft(destination)  # add to queue
+        # get neighbors locations as a value list (contains adjacent location & its travel cost) from current location
+        travel_routes = self.adjacency_list[current_loc]
+        unvisited_stations = []  # list to hold unvisited/adjacent stations
+        for destination in travel_routes:  # get destination from travel routes
+            if destination not in visited:  # if next destination/adjacent is not marked visited
+                unvisited_stations.append(destination)  # add unvisited destination to list
+                cost = travel_routes[destination]  # retrieve travel cost to destination
+                self.add_cheapest_path(current_loc, destination, cost)  # update cheapest_price_table and cheapest_previous_stop_station
+        while len(unvisited_stations) > 0:
+            cheapest_unvisited_station = min(unvisited_stations, key=self.get_travel_route_price)  # find the cheapest unvisited station
+            if cheapest_unvisited_station not in que:  # if cheapest_unvisited_station is not in queue
+                que.appendleft(cheapest_unvisited_station)  # add next unvisited station with the cheapest travel cost to queue
+            unvisited_stations.remove(cheapest_unvisited_station)  # remove unvisited station from list
+
+    def add_cheapest_path(self, current_loc, destination, cost):
+        """
+        retrieve travel cost to destination from self.cheapest_prices_table
+        if destination travel cost isn't found in table or current travel cost to destination is greater than price to destination
+            update self.cheapest_prices_table and self.cheapest_previous_stopped_station
+        :param current_loc: begin travel location
+        :param destination: travel to destination
+        :param cost: travel fee from current location to destination
+        :return: None
+        """
+        # use. get() to avoid KeyError, provide default value
+        price_to_destination = cost + self.cheapest_prices_table.get(current_loc, 0)
+        # print(f"Travel cost: {price_to_destination}")
+        if not self.cheapest_prices_table.get(destination, None) or price_to_destination < self.cheapest_prices_table[
+            destination]:
+            # add/update destination in self.cheapest_prices_table to price to destination
+            self.cheapest_prices_table[destination] = price_to_destination
+            # add destination to self.cheapest_previous_stopped_station as key and current location as value
+            self.cheapest_previous_stopped_station[destination] = current_loc
+
+    def get_travel_route_price(self, destination):
+        """
+        iterate for each station (key) in self.cheapest_prices_table and find its minimal/cheap travel cost
+        :param destination:
+        :return: destination travel cost
+        """
+        return self.cheapest_prices_table.get(destination, float('inf'))
 
     def print_graph(self):
         """
@@ -144,6 +179,6 @@ class Place:
 
 graph = 2
 p = Place.create_graph(graph)
-p.print_graph()
-print()
+# p.print_graph()
+# print()
 p.bfs_traversal("SC_Mall")
